8c58308af4d8038f3b452bdeeb445e9450d45f59
TSC\aakrasnova <lina-invers95@yandex.ru>
2022-07-29 19:06:18 +0300

    init migration

---
+++ migrations/000001_init_schema.up.sql
@@ -0,0 +1,16 @@
+CREATE TABLE users (
+	id varchar
+);
+
+CREATE TABLE knowledge (
+	id varchar,
+	name varchar,
+	adder varchar,
+	type varchar,
+	subtype varchar,
+	theme varchar,
+	sphere varchar,
+	link text,
+	word_count integer,
+	duration integer
+);
