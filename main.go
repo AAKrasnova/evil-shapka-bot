1d8d190324d73a7396628cf1248d067252b1cf11
pechorka <pechor44@gmail.com>
2022-10-21 19:46:35 +0300

    stop bot on ctrl+c

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"math/rand"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/jmoiron/sqlx"
@@ -61,15 +63,31 @@ func run() error {
 
 	rand.Seed(time.Now().Unix())
 
-	return runBot(rawDB, cfg.TgToken)
+	bot, err := runBot(rawDB, cfg.TgToken)
+	if err != nil {
+		return err
+	}
+
+	terminate := make(chan os.Signal, 1)
+	signal.Notify(terminate, syscall.SIGINT, syscall.SIGTERM)
+
+	<-terminate
+	bot.Stop()
+
+	return nil
 }
 
-func runBot(rawDB *sql.DB, token string) error {
+func runBot(rawDB *sql.DB, token string) (*Bot, error) {
 	db := sqlx.NewDb(rawDB, "sqlite3")
 	s := NewStore(db)
 	b, err := NewBot(s, token)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	return b.Run()
+	err = b.Run()
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
 }
